049c36f8c3121234e2678342626fcff92a611076
basselfa <ba.f.prosoft@gmail.com>
2025-03-25 22:14:19 +0100

    fixed music button

--- js/music-player.js
+++ js/music-player.js
@@ -1,23 +1,43 @@
-const backgroundAudio = document.getElementById("background-audio");
-const musicButton = document.getElementById("music-button");
-
+const musicButton = document.getElementById('music');
+const backgroundAudio = document.getElementById('background-audio');
 function playPauseMusic() {
-    console.log("Button geklickt");
+    console.log('Button geklickt');
     if (backgroundAudio.paused) {
-        console.log("Musik wird abgespielt");
+        console.log('Musik wird abgespielt');
         backgroundAudio.play();
+        musicButton.innerHTML = 'Pause';
     } else {
-        console.log("Musik wird pausiert");
+        console.log('Musik wird pausiert');
         backgroundAudio.pause();
+        musicButton.innerHTML = 'Play';
     }
 }
 
+console.log(backgroundAudio.play());
 backgroundAudio.addEventListener("play", () => {
     musicButton.innerHTML = "Pause";
 });
 
-backgroundAudio.addEventListener("pause", () => {
-    musicButton.innerHTML = "Play";
-});
+// const backgroundAudio = document.getElementById("background-audio");
+// const musicButton = document.getElementById("music");
 
-musicButton.addEventListener("click", playPauseMusic);
+// function playPauseMusic() {
+//     console.log("Button geklickt");
+//     if (backgroundAudio.paused) {
+//         console.log("Musik wird abgespielt");
+//         backgroundAudio.play();
+//     } else {
+//         console.log("Musik wird pausiert");
+//         backgroundAudio.pause();
+//     }
+// }
+// console.log(backgroundAudio.play());
+// backgroundAudio.addEventListener("play", () => {
+//     musicButton.innerHTML = "Pause";
+// });
+
+// backgroundAudio.addEventListener("pause", () => {
+//     musicButton.innerHTML = "Play";
+// });
+
+// musicButton.addEventListener("click", playPauseMusic);
